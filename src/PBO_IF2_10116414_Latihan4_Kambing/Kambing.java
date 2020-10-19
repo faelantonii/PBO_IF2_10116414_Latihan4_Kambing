03b605d118699890caa8828276319a8b6d2dd613
sweenutt <sweenutt@192.168.3.19>
2020-10-20 01:36:52 +0700

    Upload Latihan 1 Biodata

---
+++ src/PBO_IF2_10116414_Latihan4_Kambing/Kambing.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO_IF2_10116414_Latihan4_Kambing;
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Memasukkan Jumlah Kambing-Kambing
+ */
+public class Kambing {
+    public void tambahKambing(){
+        //Deklarasi variabel lokal
+        int jumlahKambing = 0;
+        
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("jumlah Kambing setelah ditambah: " +
+                        jumlahKambing);
+    }
+    public static void main(String[] args) {
+        Kambing kambingJantan = new Kambing();
+        kambingJantan.tambahKambing();
+       
+        
+    }    
+}
