c54669fb5641877122f2ef4a56f8d6ed5d32d530
sweenutt <sweenutt@192.168.3.19>
2020-10-20 01:46:14 +0700

    Upload Latihan 4 Memasukkan Jumlah Kambing

--- src/PBO_IF2_10116414_Latihan4_Kambing/Kambing.java
+++ src/PBO_IF2_10116414_Latihan4_Kambing/Kambing.java
@@ -24,8 +24,6 @@ public class Kambing {
     }
     public static void main(String[] args) {
         Kambing kambingJantan = new Kambing();
-        kambingJantan.tambahKambing();
-       
-        
+        kambingJantan.tambahKambing();               
     }    
 }
